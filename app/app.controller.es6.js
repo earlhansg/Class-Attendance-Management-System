bd03d44b3e98bc211204a1e9681a6fb78becbbfc
earlhansg <earlhansg@gmail.com>
2017-08-17 18:13:23 +0800

    Console Testing #2

--- app/app.controller.es6.js
+++ app/app.controller.es6.js
@@ -7,6 +7,7 @@ class AppController {
     // };
     $rootScope.$on('$stateChangeStart', function() {
         vm.loggedIn = Auth.isLoggedIn();
+        console.log('login testing');
 
         console.log("enter change route");
       });
